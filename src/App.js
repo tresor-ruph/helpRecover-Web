9041da1419ab41b56e78e1e8cad6efeb25558294
tresor-ruph <tresortek8@gmail.com>
2020-10-20 13:24:22 +0200

    deploy test 2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Hello tresor
+          Test deploy
         </p>
         <a
           className="App-link"
