c67e786463ef55003786d23773d043402b431024
tresor-ruph <tresortek8@gmail.com>
2020-10-22 09:06:52 +0200

    title changed

--- src/App.js
+++ src/App.js
@@ -5,17 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Test deploy
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+          <h1>Help Recover</h1>
+   
       </header>
     </div>
   );
